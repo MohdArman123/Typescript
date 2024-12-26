f1f6581d1a1ecaeb17cc7301534bb34d9ccc57af
Mohd Arman <armansheakh987@gmail.com>
2024-12-26 17:17:27 +0530

    concepts of typeScript and tsconfig and debug

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let lavel;
+lavel = 10;
+lavel = 'a';
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+console.log(numbers);
+let nums = [];
+nums[0] = 1;
+nums[1] = 2;
+console.log(nums);
+;
+let mySize = 2;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+// let age: number = 10;
+// if (age < 50)
+//     age += 10;
+// console.log(age);
+
+let sales : number= 123_456_789;
+let course = 'TypeScript';
+let is_published = true;
+let lavel;
+lavel = 10;
+lavel = 'a';
+
+function render(document: any) {
+    console.log(document);
+}
+
+let numbers: number[] = [1, 2, 3];
+console.log(numbers);
+let nums: number[] =[];
+nums[0] = 1;
+nums[1] = 2;
+console.log(nums);
+
+const enum Size {Small = 1, Medium, Large};
+let mySize: Size = Size.Medium
+console.log(mySize);
+
+function calculateTax(income: number): number {
+    return 0;
+}
+
+ 
